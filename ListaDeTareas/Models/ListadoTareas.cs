e672c59c23c770762ee7e0e193599a715f84c261
DevRaulH <rhoyosemp@gmail.com>
2024-04-18 19:21:09 -0500

    Se agrega: Se creo el modelo de la BD listadoDeTareas

---
+++ ListaDeTareas/Models/ListadoTareas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ListaDeTareas.Models
+{
+    public class ListadoTareas
+    {
+        public int IdTarea { get; set; }
+
+        public string Tarea { get; set; }
+
+        public string DescripcionTarea { get; set; }
+
+        public string Estado { get; set; }
+
+        public DateTime CreacionTarea { get; set; }
+
+    }
+}
